6ac4a48b14fa4ce115a3b7386aea3e5c49ef55e3
Héctor Pérez <hprez21@users.noreply.github.com>
2024-10-22 13:41:14 -0600

    Update Program.cs

--- GitHubActionsTests/Program.cs
+++ GitHubActionsTests/Program.cs
@@ -1,14 +1,16 @@
-// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, GitHub learners!");
-
 Console.WriteLine("Creating txt file");
 
-//Create a new file in the current directory.
-using (FileStream fs = File.Create("test.txt"))
+// Get the current directory and print it
+string path = Path.Combine(Directory.GetCurrentDirectory(), "test.txt");
+Console.WriteLine($"File will be created at: {path}");
+
+// Create a new file in the current directory.
+using (FileStream fs = File.Create(path))
 {
     Byte[] info = new System.Text.UTF8Encoding(true).GetBytes("This is some text in the file.");
     // Add some information to the file.
     fs.Write(info, 0, info.Length);
 }
 
-Console.WriteLine("File created successfully"); 
+Console.WriteLine("File created successfully");
